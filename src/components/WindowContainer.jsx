13248cce7381a221cd0a06edf666db5ee3d574b6
aditya-pushkar <adityapushkar850@gmail.com>
2022-10-04 17:03:51 +0530

    sidebar text hover color changed

--- src/components/WindowContainer.jsx
+++ src/components/WindowContainer.jsx
@@ -65,7 +65,7 @@ const WindowContainer = ({ children }) => {
                 <li key={key}>
                   <NavLink
                     to={`${menu.path}`}
-                    className={`relative px-4 py-3 mb-2 flex items-center space-x-4 rounded-xl  delay-100 transition ease-in hover:text-green-600 active:text-white focus:text-white`}
+                    className={`relative px-4 py-3 mb-2 flex items-center space-x-4 rounded-xl  delay-100 transition ease-in hover:text-gray-100`}
                   >
                     <i className={`${menu.iconClass} ri-lg `}></i>
                     <span className="-mr-1 ">{menu.title}</span>
