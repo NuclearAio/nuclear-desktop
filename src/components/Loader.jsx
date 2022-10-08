011373d50e16b2a9c88f6cfa3cf0ec5a450f995f
aditya-pushkar <adityapushkar850@gmail.com>
2022-10-09 02:52:56 +0530

    pre-loader added

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className="w-6 h-6 border-4 border-dashed rounded-full animate-spin border-green-600"></div>
+  )
+}
+
+export default Loader;
